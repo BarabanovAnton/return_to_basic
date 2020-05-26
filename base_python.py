31d6b25b3b4aa1b930230e34c89e858e1e1457f0
Anton Barabanov <anton.barabanov95@gmail.com>
2020-05-26 21:39:08 +0300

    Realize string and if

--- base_python.py
+++ base_python.py
@@ -1,1 +1,66 @@
 # test_commit
+# test = 21
+# print(test)
+#
+# my_true = True
+# my_false = False
+
+# str() int() float() bool()
+# x = 5.2
+# print(x, type(x))
+# x = bool(x)
+# print(x, type(x))
+
+# words = 'Hello \nworld!'
+# print(words)
+# verse = '''\
+# asas
+# asasad
+# fsafsafsfa\
+# '''
+# print(verse)
+
+# s = r'C:\d\new.txt' #сырая строка
+# print(s)
+
+# s = 'Hello world!'
+# print(s[0])
+# print(s[-1])
+
+# print(s[6:])
+# print(s[::-1])
+
+# s = 'Hello world'
+# print(len(s))
+# print(s.capitalize())
+# print(s.center(20))
+# print(s.count('l', 0, 3))
+# print(s.find('l'))
+# print(s.replace('l', 't'))
+# print(s.split())
+
+# name = 'John'
+# age = 30
+
+# print('My name is' + name + '. I\'m ' + str(age))
+# print('My name is %(name)s. I\'m %(age)d' %{'name': name, 'age': age})
+# print('My name is %s. I\'m %d' %(name, age))
+
+# format
+# print('My name is {0}. I\'m {1}'.format(name, age))
+
+# f-strings
+# print(f'My name is {name}. I\'m {age}.')
+
+x = 1
+if x == 0:
+    print('Coci zhopy')
+else:
+    print('Sam sosi')
+
+
+
+
+
+
+
