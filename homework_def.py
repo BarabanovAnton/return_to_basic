4794aca45fbf59ce6b0cb6e30fb69699b5133eac
Anton Barabanov <anton.barabanov95@gmail.com>
2020-05-31 13:55:27 +0300

    homework with def

---
+++ homework_def.py
@@ -0,0 +1,34 @@
+# def find_sum(n):
+#     sum_numb = 0
+#     for i in range(1, n+1):
+#         if i % 3 == 0 or i % 5 == 0:
+#             sum_numb += i
+#     return sum_numb
+#
+# print(find_sum(10))
+
+
+
+
+
+# def odd_ball(arr):
+#     if 'odd' in arr:
+#         if arr.index('odd') in arr:
+#             return True
+#         else:
+#             return False
+#
+# print(odd_ball(['even', 4, 'even', 7, 'even', 55, 'even', 6, 'odd', 10, 'even', 8, 'even']))
+
+
+
+
+# def get_names(names):
+#     names2 = []
+#     for name in names:
+#         if len(name) == 4:
+#             names2.append(name)
+#     return names2
+#
+# names = ['Ryan', 'Kieran', 'Mark', 'John', 'David', 'Paul']
+# print(get_names(names))
