06f9fa8a255b2aeadcd7e48b10149cb3e35a1e75
Anton Barabanov <anton.barabanov95@gmail.com>
2020-06-04 23:21:37 +0300

    learn setter and getter, def __init__

--- oop_class.py
+++ oop_class.py
@@ -1,14 +1,24 @@
-class A:
-    property1 = 'Property 1'
-    property2 = 'Property 2'
-    name = 'guest'
+# class A:
+#     property1 = 'Property 1'
+#     property2 = 'Property 2'
+#     name = 'guest'
+#
+#     def say_hi(self, name=''):
+#         if name:
+#             return f'Hi, {name}!'
+#         else:
+#             return f'Hello, {self.name}!'
+#
+# a = A()
+# print(a.property1)
+# print(a.say_hi())
 
-    def say_hi(self, name=''):
-        if name:
-            return f'Hi, {name}!'
-        else:
-            return f'Hello, {self.name}!'
+from classes import Person
+# import classes
+person1 = Person('John')
+# print(person1.get_age())
+# person1.set_age(99)
+print(person1.age)
+person1.age = 35
+person1.print_info()
 
-a = A()
-print(a.property1)
-print(a.say_hi())
